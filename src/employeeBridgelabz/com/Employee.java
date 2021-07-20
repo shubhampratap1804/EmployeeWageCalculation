31e69a16a647194a730443f6c8ea93da989d5a34
pratap1804 <pratap.1804@gmail.com>
2021-07-20 13:07:23 +0530

    Added: welcome message

---
+++ src/employeeBridgelabz/com/Employee.java
@@ -0,0 +1,11 @@
+package employeeBridgelabz.com;
+
+public class Employee {
+
+	// Main method
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to employee wage calculation!");
+
+	}
+}
