09d4e5db1bf710d36755f6f7a71d94be049d1da6
pratap1804 <pratap.1804@gmail.com>
2021-07-20 13:16:02 +0530

    Checked: attendance for the employee using random function

--- src/employeeBridgelabz/com/Employee.java
+++ src/employeeBridgelabz/com/Employee.java
@@ -7,5 +7,13 @@ public class Employee {
 	public static void main(String[] args) {
 		System.out.println("Welcome to employee wage calculation!");
 
+		// Generated an integer using random function
+		
+		int attendance = (int) Math.floor(Math.random()*10 % 2);
+		if(attendance == 1) {	
+			System.out.println("Employee is present!");	
+		} 
+		else System.out.println("Employee is absent!");
+		
 	}
 }
