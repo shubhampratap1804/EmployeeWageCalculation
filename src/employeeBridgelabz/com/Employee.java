ee93f1c67f73b39a812b8a7f6532329dcecf4776
pratap1804 <pratap.1804@gmail.com>
2021-07-20 20:44:53 +0530

    Created: a method compute wage and calling it from main method

--- src/employeeBridgelabz/com/Employee.java
+++ src/employeeBridgelabz/com/Employee.java
@@ -1,49 +1,58 @@
 package employeeBridgelabz.com;
 
 	public class Employee {
-		// Main method
-	
-		public static void main(String[] args) {
 		
 				// Declaring variables
-			
-				int wagePerHour=20;
-				int maximumWorkingDaysPerMonth =20;
-				int maximumHoursPerMonth=100;
 				
-				System.out.println("Welcome to employee wage calculation!");
-				/* Generated an integer using random function and if its equal to 1 then employee is full-time
-				   present, if its equal to 2 then employee is part-time present and if its 0 then employee is absent */ 
+				static final int wagePerHour=20;
+				static final int maximumWorkingDaysPerMonth =20;
+				static final int  maximumHoursPerMonth=100;
 				
+				//Method to calculate monthly wage
 				
-				int totalEmpHours=0;
-				int empHours = 0;
-				int totalWorkingDays=0;
-				int attendance = (int) Math.floor(Math.random()*10 % 3);
-				
-				while( totalWorkingDays <= maximumWorkingDaysPerMonth && totalEmpHours < maximumHoursPerMonth) {
-					switch(attendance) {
+				public void computeWage() {
+					int totalEmpHours=0;
+					int empHours = 0;
+					int totalWorkingDays=0;
 					
-					case 0:
-						System.out.println("Employee is absent!");;
-						empHours=0;
-						break;
+					/* Generated an integer using random function and if its equal to 1 then employee is full-time
+					   present, if its equal to 2 then employee is part-time present and if its 0 then employee is absent */ 
+					
+					int attendance = (int) Math.floor(Math.random()*10 % 3);
+					
+					while( totalWorkingDays <= maximumWorkingDaysPerMonth && totalEmpHours < maximumHoursPerMonth) {
+						switch(attendance) {
 						
-					case 1:
-						System.out.println("Employee is full-time present!");
-						empHours=8;
-						break;
-						
-					case 2:
-						System.out.println("Employee is part-time present!");
-						empHours=4;
-						break;
-						
+						case 0:
+							System.out.println("Employee is absent!");;
+							empHours=0;
+							break;
+							
+						case 1:
+							System.out.println("Employee is full-time present!");
+							empHours=8;
+							break;
+							
+						case 2:
+							System.out.println("Employee is part-time present!");
+							empHours=4;
+							break;
+							
+						}
+						totalEmpHours+=empHours;
+						totalWorkingDays++;
 					}
-					totalEmpHours+=empHours;
-					totalWorkingDays++;
+					 int totalMonthlyEmpWage = totalEmpHours * wagePerHour;
+				        System.out.println("Total monthly wage : " + totalMonthlyEmpWage);
 				}
-				 int totalMonthlyEmpWage = totalEmpHours * wagePerHour;
-			        System.out.println("Total monthly wage : " + totalMonthlyEmpWage);
-		}
+				
+				// Main method
+				public static void main(String[] args) {
+					
+					System.out.println("Welcome to employee wage calculation!");
+					//Created a Employee class object and calling the predefined method
+					
+					Employee employee = new Employee();
+					employee.computeWage();
+				}
 }
