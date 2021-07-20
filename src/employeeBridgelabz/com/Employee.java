6c79cdbbf2792e51071999f35bbd8e5fa8a6ed10
pratap1804 <pratap.1804@gmail.com>
2021-07-20 18:49:45 +0530

    Solved: using switch statement

--- src/employeeBridgelabz/com/Employee.java
+++ src/employeeBridgelabz/com/Employee.java
@@ -13,21 +13,29 @@ package employeeBridgelabz.com;
 				
 				System.out.println("Welcome to employee wage calculation!");
 				
-				/* Generated an integer using random function and checking if its equal to 1 then employee is full-time
-					present, if its equal to 2 then employee is part-time present and if its 0 then employee is absent */ 
+				/* Generated an integer using random function and if its equal to 1 then employee is full-time
+				   present, if its equal to 2 then employee is part-time present and if its 0 then employee is absent */ 
 				
 				int attendance = (int) Math.floor(Math.random()*10 % 3);
-				if(attendance == 1) {	
+				
+				switch(attendance) {
+				
+				case 0:
+					System.out.println("Employee is absent! \n"+"Total daily wage is: 0");
+					break;
+					
+				case 1:
 					System.out.println("Employee is present!");	
-					int dailyWage= wagePerHour*fullTimeHours;
-					System.out.println("Daily full time wage: "+dailyWage);
-				} 
-				else if(attendance ==2) {
+					int dailyFullTimeWage= wagePerHour*fullTimeHours;
+					System.out.println("Daily full time wage: "+dailyFullTimeWage);
+					break;
+					
+				case 2:
 					System.out.println("Employee is part time present!");
-					int dailyWage = wagePerHour*partTimeHours;
-					System.out.println("Daily part time wage is: "+dailyWage);
-				}
-				else System.out.println("Employee is absent! \n"+"Total daily wage is: 0");
-		
+					int dailyPartTimeWage = wagePerHour*partTimeHours;
+					System.out.println("Daily part time wage is: "+dailyPartTimeWage);
+					break;
+					
+				} 
 		}
-	}
+}
